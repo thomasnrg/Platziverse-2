c91c014602f904018ec6d4274718dc2410ee2cdf
thomasnrg <thomasnrgsen@gmail.com>
2018-01-09 17:35:02 -0400

    Implementación de script de inicialización de base de datos

---
+++ platiziverse-db/setup.js
@@ -0,0 +1,29 @@
+'use strict'
+
+const debug = require('debug')('platziverse:db:setup')
+const db = require('./')
+
+async function setup () {
+  const config = {
+    database: process.env.DB_NAME || 'platziverse',
+    username: process.env.DB_USERNAME || 'admin',
+    password: process.env.DB_PASSWORD || 'admin',
+    host: process.env.DB_HOST || 'localhost',
+    dialect: 'postgres',
+    logging: s => debug(s),
+    setup: true
+  }
+
+  await db(config).catch(handleFatalError)
+
+  console.log('Success!')
+  process.exit(0)
+}
+
+function handleFatalError (err) {
+  console.log(err.message)
+  console.log(err.stack)
+  process.exit(1)
+}
+
+setup()
